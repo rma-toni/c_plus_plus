0f1d9ad88c51e42d78346472d222a21a9ea1e234
rma-toni <rmaflores.sw@gmail.com>
2025-07-29 11:57:50 -0300

    Data types update - rma-toni

--- c++/data_types.cpp
+++ c++/data_types.cpp
@@ -1,68 +1,72 @@
 #include <iostream>
 
-//FUNDAMENTAL DATA TYPES
-int num = 23; //usually 4 bytes
-short int num2 = 3;
-long num3 = 41;
-long long  num4 = 15124;
+int main(){
 
-float float1 = 3.14f; //4 bytes
-double double1 = 3.1415926535; //8 bytes
+    //FUNDAMENTAL DATA TYPES
+    int num = 23; //usually 4 bytes
+    short int num2 = 3;
+    long num3 = 41;
+    long long  num4 = 15124;
 
-char letter = 'A';
+    float float1 = 3.14f; //4 bytes
+    double double1 = 3.1415926535; //8 bytes
 
-bool boolean1 = true;
+    char letter = 'A';
 
-//DERIVED DATA TYPES
+    bool boolean1 = true;
 
-//arrays
-int numbers[5] = {1, 2, 3, 4, 5};
+    //DERIVED DATA TYPES
 
-//pointers: store de memory address of a variable
-int nump = 42;
-int* pNump = &num;
+    //arrays
+    int numbers[5] = {1, 2, 3, 4, 5};
 
-//References:
-int num5 = 42;
-int& num5Ref = num5;
+    //pointers: store de memory address of a variable
+    int nump = 42;
+    int* pNump = &num;
 
-//USER DEFINED DATA TYPES
+    //References:
+    int num5 = 42;
+    int& num5Ref = num5;
 
-//structures (struct)
+    //USER DEFINED DATA TYPES
 
-struct Person {
-    std::string name;
-    int age;
-    float height;
-};
+    //structures (struct)
 
-Person p1 = {"John Doe", 30, 5.9};
-
-//classes (class)
-
-class Personclass {
-    public:
+    struct Person {
         std::string name;
         int age;
-    
-    void printInfo() {
-        std::cout << "Name: " << name << ", Age: " << age << '\n';
+        float height;
     };
-};
 
-Personclass p2;
-p2.name = "John Doe";
-p2.age = 30;
+    Person p1 = {"John Doe", 30, 5.9};
 
-//These errors are because this code is being used globally, something not allowed in C++.
+    //classes (class)
+
+    class Personclass {
+        public:
+            std::string name;
+            int age;
+        
+        void printInfo() {
+            std::cout << "Name: " << name << ", Age: " << age << '\n';
+        };
+    };
+
+    Personclass p2;
+    p2.name = "John Doe";
+    p2.age = 30;
+
+    //These errors are because this code is being used globally, something not allowed in C++.
 
 
-//Unions
-union Data {
-    int num;
-    char letter;
-    float decimal;
-};
+    //Unions
+    union Data {
+        int num;
+        char letter;
+        float decimal;
+    };
 
-Data myData;
-myData.num = 42;
+    Data myData;
+    myData.num = 42;
+    
+}
